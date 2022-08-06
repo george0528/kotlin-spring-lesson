30b51f8a90e0fc98902f656db2092489d189cc78
jota <aleph0528@gmail.com>
2022-08-06 15:21:38 +0900

    update: APIの処理追加

--- springboot/src/main/kotlin/com/example/springboot/controller/ApiController.kt
+++ springboot/src/main/kotlin/com/example/springboot/controller/ApiController.kt
@@ -1,4 +1,31 @@
-package main.kotlin.com.example.springboot.controller
+package com.example.springboot.controller
 
+import com.example.springboot.entity.User
+import com.example.springboot.repository.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
 class ApiController {
+    @Autowired
+    lateinit var userRepository: UserRepository
+
+    @GetMapping("")
+    fun getApi(): String {
+        return "ハロー a"
+    }
+
+    @GetMapping("/users")
+    fun getUsers(): Iterable<User> {
+        return userRepository.findAll()
+    }
+    
+    @GetMapping("/param")
+    fun getParam(@RequestParam param: String): String {
+        return "[${param}] がリクエストされました"
+    }
 }
