76593d69d2c71766acb45e71dceef274c6d69a13
jota <aleph0528@gmail.com>
2022-08-08 17:06:55 +0900

    update: vueのsample処理を追加

--- frontend/nuxt-app/pages/test.vue
+++ frontend/nuxt-app/pages/test.vue
@@ -4,5 +4,20 @@
     <NuxtLink to="/">
       indexです
     </NuxtLink>
+    <input v-model="name" type="text" />
+    <p>nameの値は{{ name }}</p>
+    <p>numは{{ num }}</p>
+    <button @click="double()">倍にする</button>
   </div>
 </template>
+<script setup>
+  const name = ref('Test') // 変数
+  const num = ref(1)
+  const double = () => {
+    num.value *= 2
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
