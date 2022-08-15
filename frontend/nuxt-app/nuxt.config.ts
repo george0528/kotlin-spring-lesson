64c391f68e1197eb4c5ecc86a6b9f989ecbe954a
jota <aleph0528@gmail.com>
2022-08-15 19:21:13 +0900

    update: nuxtのgraphqlの設定

--- frontend/nuxt-app/nuxt.config.ts
+++ frontend/nuxt-app/nuxt.config.ts
@@ -2,5 +2,17 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    '@nuxtjs/apollo',
+  ],
 
+  // Apollo module configuration
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: 'https://graphql-pokemon2.vercel.app/',
+      }
+    }
+  },
 })
